606433a2a8ef2637a190eb63a382483ab14b02f9
sodiqadewoyin1 <113023208+sodiqadewoyin1@users.noreply.github.com>
2022-09-21 11:45:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ def form():
 
     return render_template('form.html')
 
+
+@app.route('/Form job')
+def Formjob():
+
+    return render_template('Form job.html')
+
 if __name__=='__main__':
     app.run(debug=True)
 
